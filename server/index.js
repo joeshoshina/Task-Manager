79a3789662ea68eee1f51adfe37d5c5d9be3eebe
Joe Hoshina <jhoshina@uci.edu>
2025-06-23 02:30:07 -0700

    Password hashing is now implemented

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 import express from "express";
 import cors from "cors";
-import { createUser } from "./database.js"; // Adjust the import path as necessary
+import bcrypt from "bcrypt";
+import { createUser } from "./database.js";
 
 const app = express();
-const PORT = process.env.PORT || 8080; // Default port for local development
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
 
-// hash password befor storing in database
 app.post("/api/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
   if (!email || !password || !firstName || !lastName) {
     return res.status(400).json({ error: "All fields are required." });
   }
   try {
-    const userId = await createUser(email, password, firstName, lastName);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userId = await createUser(email, hashedPassword, firstName, lastName);
     res.status(201).json({ message: "User created", userId });
   } catch (error) {
     res.status(500).json({ error: "Error creating user." });
