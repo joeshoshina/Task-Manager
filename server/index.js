f1fa26036347ac3da5f7790d27ce7e40708e41cd
Joe Hoshina <jhoshina@uci.edu>
2025-06-29 02:31:14 -0700

    Implement JWT auth with HttpOnly cookie; drop localStorage approach
    Set up backend JWT middleware and verification endpoint. Currently only applied to signin route. Frontend integration and updates to other routes pending for next session.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,49 @@
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 import { createUser, getUser } from "./database.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // for cookies
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  //   const authHeader = req.headers["authorization"]; //authorization: ;Bearer <token>'
+  //   const token = authHeader && authHeader.split(" ")[1]; //['Bearer', '<token>']
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({ auth: false, error: "Access token is required." });
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ error: "Invalid token." });
+      }
+      req.userId = decoded.id;
+      next();
+    });
+  }
+};
+
+// endpoint to check user authentication
+app.get("/api/auth", verifyJWT, (req, res) => {
+  res.json({
+    auth: true,
+    message: "User is authenticated",
+    userId: req.userId,
+  });
+});
 
 app.post("/api/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -34,6 +70,24 @@ app.post("/api/signin", async (req, res) => {
   }
   try {
     const user = await getUser(email);
+
+    /* adding JWT logic here */
+    // access and refresh tokens have to be implemented later on
+    const id = user.id;
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // COME BACK FOR .ENV MAYBE AS NODE_ENV === 'production'
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    const { password: _, ...userSafe } = user;
+    res.status(200).json({ auth: true, user: userSafe });
+
     if (!user) {
       return res.status(404).json({ error: "User not found." });
     } else if (await bcrypt.compare(password, user.password)) {
